9f73497feee41bd228c2c91597297ce42a582919
Nathalia Rus <nathaliarus@live.fr>
2019-09-26 16:09:18 +0100

    root action created

---
+++ src/redux/store/root-action.ts
@@ -0,0 +1,7 @@
+import { routerActions } from 'react-router-redux';
+import * as todoActions from '../modules/todos/actions';
+
+export default {
+  router: routerActions,
+  todos: todoActions,
+};
