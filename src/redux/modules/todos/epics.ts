9dfd2a81c2fc737086346b33e55f7bb8b7e2037b
Nathalia Rus <nathaliarus@live.fr>
2019-09-26 16:06:43 +0100

    second epic doneé

--- src/redux/modules/todos/epics.ts
+++ src/redux/modules/todos/epics.ts
@@ -25,3 +25,23 @@ export const loadTodosEpic: Epic<
     )
   );
 };
+
+
+export const saveTodosEpic: Epic<
+  RootAction,
+  RootAction,
+  RootState,
+  Services
+> = ( action$, state$, {api}) => {
+action$.pipe(
+  filter(isActionOf(saveTodosAsync.request)),
+  switchMap(() => 
+    from(api.todos.saveSnapshot(state$.value.todos)))
+    .pipe(
+      map(saveTodosAsync.success),
+      catchError( (message: string)  => of(saveTodosAsync.failure(message)))
+    )
+  )
+)
+
+};
