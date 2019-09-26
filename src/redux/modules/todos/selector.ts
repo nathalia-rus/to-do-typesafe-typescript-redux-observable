df882f65c8591694905eea493bebfaf97b11e195
Nathalia Rus <nathaliarus@live.fr>
2019-09-26 15:41:10 +0100

    onto services and epics

--- src/redux/modules/todos/selector.ts
+++ src/redux/modules/todos/selector.ts
@@ -1,6 +1,6 @@
 // either already crated, eother not:
 // but need here to import the state of the todo from the reducer.
 
-import { TodoState } from './reducer';
+import { TodosState } from './reducer';
 
-export const getTodos = (state: TodoState) => state.todos;
+export const getTodos = (state: TodosState) => state.todos;
