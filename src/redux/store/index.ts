e40431677bb0f79808bd5b04f72f6c44fef64c47
Nathalia Rus <nathaliarus@live.fr>
2019-09-26 14:34:08 +0100

    create epicMiddleware

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,3 +1,19 @@
 import { createStore, applyMiddleware } from 'redux';
 import { createEpicMiddleware } from 'redux-observable';
+// import from module created in types.d.ts gets imported
 import { RootAction, RootState, Services } from 'typesafe-actions';
+
+import { composeEnhancers } from './utils';
+
+import rootReducer from './root-reducer';
+import rootAction from './root-action';
+import RootEpic from './root-epic';
+
+export const epicMiddleware = createEpicMiddleware<
+  RootAction,
+  RootAction,
+  RootState,
+  Services
+>({
+  dependencies: Services,
+});
