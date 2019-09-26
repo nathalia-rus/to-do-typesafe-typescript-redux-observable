9471c424a62e19517c10eb124e68d7870976e4ae
Nathalia Rus <nathaliarus@live.fr>
2019-09-26 15:53:38 +0100

    epic - imports

---
+++ src/redux/modules/todos/epics.ts
@@ -0,0 +1,10 @@
+// mix w/ what of my module added to it
+import { RootAction, RootState, Services, isActionOf } from 'typesafe-actions';
+// imports from lib
+import { from, of } from 'rxjs';
+import { Epic } from 'redux-observable';
+import { filter, switchMap, catchError, map } from 'rxjs/operators';
+// async actions
+import { loadTodosAsync, saveTodosAsync } from './actions';
+// get the todos already stored in state from the selector
+import { getTodos } from './selector';
